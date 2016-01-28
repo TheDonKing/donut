54f29197262f953c0a92281883b83714f0b393c7
TheDonKing <starspersona@gmail.com>
2016-01-28 14:17:05 +0300

    Удаление пончиков и кофе

--- index.php
+++ index.php
@@ -87,22 +87,30 @@
 <![endif]-->
 
 <header class="page1" >
-        <div class="container">
+    <div class="container">
             <div class="row">
-                <div class="col-md-12 animated slideInDown ">
-                    <nav>
-                        <ul >
-                            <li><a class="active button button--ujarak button--border-thin button--text-thick" href="#">Главная</a></li>
-                            <li><a class="button button--ujarak button--border-thin button--text-thick" href="#">Что мы делаем</a></li>
-                            <li><a class="button button--ujarak button--border-thin button--text-thick" href="#">Продукты</a></li>
-                            <li><a class="button button--ujarak button--border-thin button--text-thick" href="#">О Нас</a></li>
-                            <li><a class="button button--ujarak button--border-thin button--text-thick" href="#">Контакты</a></li>
+                <div class="col-md-12 animated slideInDown navbar  ">
+                    <div class="navbar-header n navbar-fixed-top">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#responsive-menu">
+                            <span class="sr-only">Открыть навигацию</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <nav class="collapse navbar-collapse " id="responsive-menu">
+                        <ul class="nav navbar-nav">
+                            <li><a class="scroll active button button--ujarak button--border-thin button--text-thick" href="#">Главная</a></li>
+                            <li><a class="scroll button button--ujarak button--border-thin button--text-thick" href="#what">Что мы делаем</a></li>
+                            <li><a class="scroll button button--ujarak button--border-thin button--text-thick" href="#production">Продукция</a></li>
+                            <li><a class="scroll button button--ujarak button--border-thin button--text-thick" href="#team">О Нас</a></li>
+                            <li><a class="scroll button button--ujarak button--border-thin button--text-thick" href="#contacts">Контакты</a></li>
                         </ul>
                     </nav>
                 </div>
                 <div class="header col-md-12">
                     <img class="animated flipInY" src="img/logo.png" alt=""/>
-                    <h2 class="animated flipInY col-md-8 col-md-offset-2">Лучшие друзья  пончиков это - кофе</h2>
+                    <h2 class="animated flipInY col-md-8 col-md-offset-2">Ponchos - твой лучший друг</h2>
                 </div>
                 <div class="animated slideInUp more">
                     <a class=" scroll" href="#2"><img src="img/more.png" alt=""/></a>
@@ -112,7 +120,7 @@
         <div class="bg"></div>
     </header>
 
-    <section id="what" class="what">
+    <section id="what" class="what hww">
         <div class="container">
             <div class="row">
                 <div class="upBox col-md-12">
@@ -125,18 +133,13 @@
                         <p class="descr">Слово «пончик» имеет множество значений: от обозначения воздушной жареной пышки до обидного прозвища толстушек. Пончик как кондитерское изделие представляет собой обжаренный во фритюре кусок дрожжевого теста, пышку, непременно сладкую и часто с начинкой внутри. В разных частях планеты пончиками называют пирожки, дрожжевые пирожные, крендели с дырочкой в середине или пухлые булочки. Вкус этой выпечки ассоциируется с детством и достатком, гармонией и сытостью. </p>
                     </div>
                 </div>
-                <div class="donut col-md-12 ">
-                    <div class="item col-lg-4 ">
+                <div class="donut col-md-12 scroll-animate ">
+                    <div class="item col-lg-6 animation ">
                         <img src="img/donut_1.png" alt=""/>
                         <h3>Традиционный пончик</h3>
                         <p>Представляет собой золотистый шарик. Пышка имеет форму кольца. Пышки и пончики готовятся в большом количестве масла и подаются горячими.</p>
                     </div>
-                    <div class="item col-lg-4 ">
-                        <img src="img/donut_2.png" alt=""/>
-                        <h3>Берлинский пончик</h3>
-                        <p>Европейский пончик, особо популярный в Австрии и Германии. Готовится он из сладкого дрожжевого теста, путем обжаривания во фритюре с 2 сторон, переворачивая в процессе. В результате по окружности получается светлая полоса. Берлинеры обычно начиняют конфитюром или кремом, а сверху посыпают сахарной пудрой и поливают глазурью.</p>
-                    </div>
-                    <div class="item col-lg-4 ">
+                    <div class="item col-lg-6 animation ">
                         <img src="img/donut_3.png" alt=""/>
                         <h3>Донат</h3>
                         <p>Американский пончик. Имеет форму кольца. Жарят донаты, погружая во фритюр. Донаты, как и берлинеры, готовят с разнообразными начинками (джем, заварной или белковый крем), а сверху покрывают глазурью или посыпают сахарной пудрой, орехами, кокосовой стружкой.</p>
@@ -146,7 +149,7 @@
         </div>
     </section>
 
-    <section id="production" class="production">
+    <section id="production" class="production hww">
         <div class="container">
             <div class="row">
                 <div class="box col-lg-12">
@@ -158,37 +161,37 @@
                     </div>
 
                 </div>
-                <main class="cd-main-content">
-                    <section class="cd-gallery">
+                <main class="cd-main-content hww">
+                    <section class="cd-gallery scroll-animate">
                         <ul>
-                            <li class="mix"><img src="img/portfolio/1.png" alt="Image 1"></li>
-                            <li class="mix"><img src="img/portfolio/2.png" alt="Image 2"></li>
-                            <li class="mix"><img src="img/portfolio/3.png" alt="Image 3"></li>
-                            <li class="mix"><img src="img/portfolio/4.png" alt="Image 4"></li>
-                            <li class="mix"><img src="img/portfolio/5.png" alt="Image 5"></li>
-                            <li class="mix"><img src="img/portfolio/6.png" alt="Image 6"></li>
-                            <li class="mix"><img src="img/portfolio/7.png" alt="Image 7"></li>
-                            <li class="mix"><img src="img/portfolio/8.png" alt="Image 8"></li>
-                            <li class="mix"><img src="img/portfolio/9.png" alt="Image 9"></li>
-                            <li class="mix"><img src="img/portfolio/10.png" alt="Image 10"></li>
-                            <li class="mix"><img src="img/portfolio/11.png" alt="Image 11"></li>
-                            <li class="mix"><img src="img/portfolio/12.png" alt="Image 12"></li>
-                            <li class="mix"><img src="img/portfolio/13.png" alt="Image 12"></li>
-                            <li class="mix"><img src="img/portfolio/14.png" alt="Image 12"></li>
-                            <li class="mix"><img src="img/portfolio/15.png" alt="Image 12"></li>
-                            <li class="mix"><img src="img/portfolio/16.png" alt="Image 12"></li>
-                            <li class="mix"><img src="img/portfolio/17.png" alt="Image 12"></li>
-                            <li class="mix"><img src="img/portfolio/18.png" alt="Image 12"></li>
-                            <li class="gap"></li>
-                            <li class="gap"></li>
-                            <li class="gap"></li>
+                            <li class="mix animation"><img src="img/portfolio/1.png" alt="Image 1"></li>
+                            <li class="mix animation"><img src="img/portfolio/2.png" alt="Image 2"></li>
+                            <li class="mix animation"><img src="img/portfolio/3.png" alt="Image 3"></li>
+                            <li class="mix animation"><img src="img/portfolio/4.png" alt="Image 4"></li>
+                            <li class="mix animation"><img src="img/portfolio/5.png" alt="Image 5"></li>
+                            <li class="mix animation"><img src="img/portfolio/6.png" alt="Image 6"></li>
+                            <li class="mix animation"><img src="img/portfolio/7.png" alt="Image 7"></li>
+                            <li class="mix animation"><img src="img/portfolio/8.png" alt="Image 8"></li>
+                            <li class="mix animation"><img src="img/portfolio/9.png" alt="Image 9"></li>
+                            <li class="mix animation"><img src="img/portfolio/10.png" alt="Image 10"></li>
+                            <li class="mix animation"><img src="img/portfolio/11.png" alt="Image 11"></li>
+                            <li class="mix animation"><img src="img/portfolio/12.png" alt="Image 12"></li>
+                            <li class="mix animation"><img src="img/portfolio/13.png" alt="Image 12"></li>
+                            <li class="mix animation"><img src="img/portfolio/14.png" alt="Image 12"></li>
+                            <li class="mix animation"><img src="img/portfolio/15.png" alt="Image 12"></li>
+                            <li class="mix animation"><img src="img/portfolio/16.png" alt="Image 12"></li>
+                            <li class="mix animation"><img src="img/portfolio/17.png" alt="Image 12"></li>
+                            <li class="mix animation"><img src="img/portfolio/18.png" alt="Image 12"></li>
+                            <li class="gap animation"></li>
+                            <li class="gap animation"></li>
+                            <li class="gap animation"></li>
                         </ul>
                     </section> <!-- cd-gallery -->
                 </main> <!-- cd-main-content -->
             </div>
         </div>
     </section>
-<section class="team">
+<section id="team" class="team hww">
     <div class="container">
         <div class="row">
             <div class="upBox col-md-12">
@@ -202,21 +205,22 @@
                     </p>
                 </div>
             </div>
-            <div class="team-item col-md-12 ">
-                <div class="item col-lg-4 ">
-                    <img src="img/donut_1.png" alt=""/>
-                    <h3>Традиционный пончик</h3>
-                    <p>Представляет собой золотистый шарик. Пышка имеет форму кольца. Пышки и пончики готовятся в большом количестве масла и подаются горячими.</p>
+            <div class="team-item col-md-12 scroll-animate ">
+                <div class="item col-lg-4 animation ">
+                    <img src="img/team_1.png" alt=""/>
+                    <h3>75% <br><span class="a1">мука</span></h3>
+                    <p>Неотъемлемым ингредиентом теста для пончиков являются: мука. Тесто можно приготовить с добавлением кефира или молока. Для пышности в тесто добавляют немного гашеной уксусом соды, а для вкуса - несколько граммов соли.</p>
                 </div>
-                <div class="item col-lg-4 ">
-                    <img src="img/donut_2.png" alt=""/>
-                    <h3>Берлинский пончик</h3>
-                    <p>Европейский пончик, особо популярный в Австрии и Германии. Готовится он из сладкого дрожжевого теста, путем обжаривания во фритюре с 2 сторон, переворачивая в процессе. В результате по окружности получается светлая полоса. Берлинеры обычно начиняют конфитюром или кремом, а сверху посыпают сахарной пудрой и поливают глазурью.</p>
+                <div class="item col-lg-4 animation ">
+                    <img src="img/team_2.png" alt=""/>
+                    <h3>15% <br><span>молоко</span></h3>
+                    <p>Это самый простой ингридиент, для пончиков. С молоком сами пончики получаются нежными, тоненькими, с небольшим количеством дырочек. Это самый лучший вариант, если пончик должен быть всего лишь оболочкой для вкусной, интересной начинки. Так-же можно приготовить пончик на кефире или простокваше.
+                    </p>
                 </div>
-                <div class="item col-lg-4 ">
-                    <img src="img/donut_3.png" alt=""/>
-                    <h3>Донат</h3>
-                    <p>Американский пончик. Имеет форму кольца. Жарят донаты, погружая во фритюр. Донаты, как и берлинеры, готовят с разнообразными начинками (джем, заварной или белковый крем), а сверху покрывают глазурью или посыпают сахарной пудрой, орехами, кокосовой стружкой.</p>
+                <div class="item col-lg-4 animation ">
+                    <img src="img/team_3.png" alt=""/>
+                    <h3>10% <br><span>остальные ингридиенты</span></h3>
+                    <p>Пончик могут состоять из остальный игридиенты таких как: воды, сахара, подсолнечного масла, сахарной пудры для присыпки, кефира, сгущенного молока, творога, яйца куриного, джема.</p>
                 </div>
             </div>
         </div>
@@ -224,9 +228,62 @@
 </section>
 
 
-<footer>
-    <p>Copyright © 2008 All Rights<p>
+<footer id="contacts">
+    <div class="container">
+        <div class="row">
+            <div class="box col-lg-12">
+                <div class="left col-lg-7"></div>
+                <div class="right col-lg-5 ">
+                    <h2>Контакты</h2>
+                    <hr class="a1"/>
+                    <hr class="a2"/>
+                </div>
+            </div>
+            <div class="form col-lg-6">
+                <form action="#">
+                    <div class="form-group">
+                        <label for="exampleInputName">Имя</label>
+                        <input title="Введите ваше имя" type="text" class="form-control" id="exampleInputName2" >
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail">Email</label>
+                        <input title="Введите ваш email" type="email" class="form-control" id="exampleInputEmail2" >
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputTextarea">Сообщение</label>
+                        <textarea title="Напишите сообщение" class="form-control" rows="3"></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-default">Отправить</button>
+                </form>
+            </div>
+            <div class="contacts col-lg-6">
+                <div class="social">
+                    <a href="https://www.instagram.com/ponchos/" rel="nofollow" target="_blank"><img src="img/soc/1.png" title="facebook" alt="Facebook"></a>
+                    <a href="#"><img src="img/soc/2.png" alt="Twitter"></a>
+                    <a href="#"><img src="img/soc/3.png" alt="Google+"></a>
+                    <a href="#"><img src="img/soc/4.png" alt="Pinterest"></a>
+                    <a href="#"><img src="img/soc/5.png" alt="Indenting"></a>
+                </div>
+                <div class="contacts-txt">
+                    <p>Адрес: <br>
+                        <span>г. Махачкала Степной посёлок ул. Азизова 5а</span>
+                    </p>
+                    <p>Телефон: <br>
+                        <span>8928 986 06 81</span>
+                    </p>
+                    <p>Email: <br>
+                        <span>maga31385@mail.ru</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="map">
+        <script type="text/javascript" charset="utf-8" src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=79NpS4T_n6ScO2hql3lpS-aPxJ_CyURE&width=100%&height=486&lang=ru_RU&sourceType=constructor"></script>
+    </div>
 </footer>
+
 
 
 
@@ -235,7 +292,7 @@
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.6.0/jquery.nicescroll.min.js"></script>
 <script type="text/javascript" src="libs/respond/respond.min.js"></script>
-
+<script type="text/javascript" src="libs/animation/scripts.js"></script>
 <script type="text/javascript" src="libs/mixtup/jquery.mixitup.min.js"></script>
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.pack.js"></script>
 <script type="text/javascript" src="libs/landing-nav/navigation.js"></script>
